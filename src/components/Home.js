faa884eb048378ef8bf2a455e811dd2d9f4f7dbb
rohitsingh2306 <99877302+rohitsingh2306@users.noreply.github.com>
2022-09-14 21:51:46 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { UserContext } from '../context/userContext';
+import React from "react";
+import { UserContext } from "../context/userContext";
 
 // do not change this component
 export const Home = () => {
-    const {greeting} = React.useContext(UserContext)
+  const { greeting } = React.useContext(UserContext);
 
-    return (
-        <div style={{border:'6px solid yellow',padding:'8px', marginTop:'1rem'}} id="home">
-            <h4>Home</h4>
-            {`Current greeting as per setting is:- ${greeting}`}
-        </div>
-    )
-}
+  return (
+    <div
+      style={{ border: "6px solid yellow", padding: "8px", marginTop: "1rem" }}
+      id="home"
+    >
+      <h4>Home</h4>
+      {`Current greeting as per setting is:- ${greeting}`}
+    </div>
+  );
+};
